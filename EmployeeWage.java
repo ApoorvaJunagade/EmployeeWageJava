79c0d7af7141ba0a9b0f5cef51d8fa6f7f1fb1ed
ApoorvaJunagade <apoorva.junagade@gmail.com>
2020-05-11 17:46:41 +0530

    employee daily wage usecase added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,18 @@
 public class EmployeeWage{
+static int WagePerHour = 20;
+static int FullDayHour = 8;
+static int ispresent=1;
+static double status=Math.floor(Math.random()*10)%2;
+static int salary;
+
 	public static void main(String[] args){
 		System.out.println("Welcome to employee wage computation program!");
-		int ispresent=1;
-		double status=Math.floor(Math.random()*10)%2;
-		if ( status == ispresent )
-			System.out.println("employee is present");
+                 
+
+		if ( status == ispresent ){
+			salary=WagePerHour*FullDayHour;
+			System.out.println("employee is present wage for one day= "+salary);
+		}
 		else
  			System.out.println("employee is absent");
 		}
