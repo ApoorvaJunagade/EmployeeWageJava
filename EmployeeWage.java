48565c2d03ee0e4dfcac89f051b6f7647cdc41bd
ApoorvaJunagade <apoorva.junagade@gmail.com>
2020-05-11 17:13:23 +0530

    employee attendance usecase added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,11 @@
 public class EmployeeWage{
 	public static void main(String[] args){
 		System.out.println("Welcome to employee wage computation program!");
-	}
+		int ispresent=1;
+		double status=Math.floor(Math.random()*10)%2;
+		if ( status == ispresent )
+			System.out.println("employee is present");
+		else
+ 			System.out.println("employee is absent");
+		}
 }
